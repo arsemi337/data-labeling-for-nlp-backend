de699ef6a477abaab405c7b7815bd08e58ff0f27
Arsemi337 <arturmilosz@onet.pl>
2022-10-16 11:04:07 +0200

    Swagger added to the project

--- src/main/java/it/winter2223/bachelor/ak/backend/comments/controller/CommentsController.java
+++ src/main/java/it/winter2223/bachelor/ak/backend/comments/controller/CommentsController.java
@@ -1,5 +1,7 @@
 package it.winter2223.bachelor.ak.backend.comments.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import it.winter2223.bachelor.ak.backend.comments.service.CommentsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -8,12 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/comments")
+@Tag(name = "Conference")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 class CommentsController {
     private final CommentsService commentsService;
 
-    @GetMapping
+    @GetMapping("/comments")
+    @Operation(summary = "Test comments")
     ResponseEntity<String> testController() {
         return ResponseEntity.ok().body(commentsService.testMethod());
     }
