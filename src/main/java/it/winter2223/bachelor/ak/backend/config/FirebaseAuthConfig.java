049e5e38dd9584a6c6954669fec5e7e7ae2951f2
Arsemi337 <arturmilosz@onet.pl>
2022-10-24 20:58:33 +0200

    profiles added

--- src/main/java/it/winter2223/bachelor/ak/backend/config/FirebaseAuthConfig.java
+++ src/main/java/it/winter2223/bachelor/ak/backend/config/FirebaseAuthConfig.java
@@ -7,11 +7,13 @@ import com.google.firebase.auth.FirebaseAuth;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.Resource;
 
 import java.io.IOException;
 
 @Configuration
+@Profile("production")
 public class FirebaseAuthConfig {
     @Value("classpath:service-account.json")
     Resource serviceAccount;
