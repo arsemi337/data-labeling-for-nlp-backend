514afe79b26ae5f8ff5e14b37bec61f77553a0f1
Arsemi337 <arturmilosz@onet.pl>
2023-01-11 22:55:56 +0100

    typo fixed 3

--- src/main/java/it/winter2223/bachelor/ak/backend/comment/exception/CommentExceptionMessages.java
+++ src/main/java/it/winter2223/bachelor/ak/backend/comment/exception/CommentExceptionMessages.java
@@ -6,7 +6,7 @@ public enum CommentExceptionMessages {
     COMMENTS_NUMBER_OUT_OF_RANGE("Comments number needs to be a number between 1 and 100"),
     CANNOT_COMPARE_NULL_COMMENT("Null comment object cannot be compared"),
     VIDEOS_FETCHING_ERROR("Error while fetching YouTube videos list"),
-    COMMENTS_FETCHING_ERROR("Error while fetching You Tube comments list of video with id: ");
+    COMMENTS_FETCHING_ERROR("Error while fetching YouTube comments list of video with id: ");
 
     private final String message;
 
