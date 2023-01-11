564d3e07b4587bc4f6e27e42219f1edd29f7b457
Arsemi337 <arturmilosz@onet.pl>
2023-01-11 22:51:47 +0100

    typo fixed

--- src/main/java/it/winter2223/bachelor/ak/backend/comment/exception/CommentExceptionMessages.java
+++ src/main/java/it/winter2223/bachelor/ak/backend/comment/exception/CommentExceptionMessages.java
@@ -4,7 +4,7 @@ public enum CommentExceptionMessages {
     NO_COMMENT_WITH_ENTERED_ID("There is no comment with entered id"),
     COMMENTS_NUMBER_IS_NOT_INTEGER("Comments number is not an integer value"),
     COMMENTS_NUMBER_OUT_OF_RANGE("Comments number needs to be a number between 1 and 100"),
-    CANNOT_COMPARE_NULL_COMMENT("Null comment object cannot be compare"),
+    CANNOT_COMPARE_NULL_COMMENT("Null comment object cannot be compared"),
     VIDEOS_FETCHING_ERROR("Error while fetching You Tube videos list"),
     COMMENTS_FETCHING_ERROR("Error while fetching You Tube comments list of video with id: ");
 
